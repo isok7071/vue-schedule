a06b1300cc4767f12567fe5a9b100d4fb9240645
isok <isok7071@gmail.com>
2023-07-28 15:42:08 +0300

    Добавляет обовления по иконкам и надписям

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <main-page />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import MainPage from '@/components/MainPage.vue'
 </script>
---
+++ src/components/GroupsChips.vue
@@ -0,0 +1,40 @@
+        
+<template>
+    <v-chip-group
+        selected-class="text-primary"
+        column
+    >
+        <template
+            v-for="id, group in groups"
+            :key="id"
+        >
+
+            <v-chip
+                filter
+                @click="store.toggleSelected(group)"
+            >
+                {{ group }}
+            </v-chip>
+        </template>
+
+
+
+        <v-progress-linear
+            indeterminate
+            color="primary"
+            :height="10"
+            v-if="isLoading"
+        ></v-progress-linear>
+    </v-chip-group>
+</template>
+
+<script lang="ts" setup>
+import { useAppStore } from '@/store/app';
+import { storeToRefs } from 'pinia';
+
+const store = useAppStore()
+const { isLoading, groups } = storeToRefs(store)
+
+store.getGroups()
+
+</script>
